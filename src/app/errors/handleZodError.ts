3381da21b0c113017646dc04004ac988d7a15549
Mohammad_Zakaria_Yunus <mohammadzakariaabdullahyunus99@gmail.com>
2023-12-24 00:10:18 +0600

    90% Done

--- src/app/errors/handleZodError.ts
+++ src/app/errors/handleZodError.ts
@@ -1,16 +1,21 @@
-class AppError extends Error {
-  public statusCode: number
+import { ZodError, ZodIssue } from 'zod'
+import { TErrorSources, TGenericErrorResponse } from '../interface/error'
 
-  constructor(statusCode: number, message: string, stack = '') {
-    super(message)
-    this.statusCode = statusCode
-
-    if (stack) {
-      this.stack = stack
-    } else {
-      Error.captureStackTrace(this, this.constructor)
+const handleZodError = (err: ZodError): TGenericErrorResponse => {
+  const errorSources: TErrorSources = err.issues.map((issue: ZodIssue) => {
+    return {
+      path: issue?.path[issue.path.length - 1],
+      message: issue.message,
     }
+  })
+
+  const statusCode = 400
+
+  return {
+    statusCode,
+    message: 'Validation Error',
+    errorSources,
   }
 }
 
-export default AppError
+export default handleZodError
